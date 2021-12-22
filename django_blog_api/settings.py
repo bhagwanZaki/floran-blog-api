39e0306563aab76d2419c11484a7b0e922ca1307
bhagwanZaki <zaki.tarveen@gmail.com>
2021-12-22 23:08:31 +0530

    static and staticfiles added

--- django_blog_api/settings.py
+++ django_blog_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('FLORAN_DJANGO_API')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','floran-blog-api.herokuapp.com']
+ALLOWED_HOSTS = ['floran-blog-api.herokuapp.com']
 
 
 # Application definition
